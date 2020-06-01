802a03962a1f9eb30dd1fa609b65c7412844654a
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-06-01 12:08:34 +0200

    Create langs_traductor.py

---
+++ langs_traductor.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import tkinter.scrolledtext as scrolledtext
+from tkinter import messagebox, filedialog
+import pyttsx3
+from googletrans import Translator
+
+ventana = Tk()
+ventana.title("Traductor")
+ventana.geometry('1050x700')
+
+ventana.mainloop()
