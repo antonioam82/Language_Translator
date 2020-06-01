a9e172735577e713f90c356254fb71f96513d9e7
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-06-01 12:28:58 +0200

    Update langs_traductor.py

--- langs_traductor.py
+++ langs_traductor.py
@@ -7,8 +7,10 @@ from googletrans import Translator
 ventana = Tk()
 ventana.title("Traductor")
 ventana['bg'] = 'light blue'
-ventana.geometry('1050x700')
+ventana.geometry('1090x700')
 display1 = scrolledtext.ScrolledText(ventana,width=55,height=29)
-display1.place(x=30,y=70)
+display1.place(x=30,y=77)
+display2 = scrolledtext.ScrolledText(ventana,width=55,height=29)
+display2.place(x=600,y=77)
 
 ventana.mainloop()
