6478aeb90042319ea73468e313a8f0309510785c
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-06-02 10:24:57 +0200

    Update langs_traductor.py

--- langs_traductor.py
+++ langs_traductor.py
@@ -8,6 +8,16 @@ ventana = Tk()
 ventana.title("Traductor")
 ventana['bg'] = 'light blue'
 ventana.geometry('1101x490')
+translator = Translator()
+traduc = ""
+
+def traduce():
+    global traduc
+    display2.delete('1.0',END)
+    traduc = translator.translate(display1.get('1.0',END),dest='ja')
+    display2.insert(END,traduc.text)
+    traduc = ""
+    
 display1 = scrolledtext.ScrolledText(ventana,width=55,height=18)
 display1.place(x=30,y=77)
 display2 = scrolledtext.ScrolledText(ventana,width=55,height=18)
@@ -20,5 +30,10 @@ label1 = Label(ventana,text="TEXTO",bg="light blue",width=64)
 label1.place(x=30,y=53)
 label2 = Label(ventana,text="TRADUCCIÓN",bg="light blue",width=64)
 label2.place(x=610,y=53)
+btnTans = Button(ventana,text='TRADUCIR',command=traduce)
+btnTans.place(x=516,y=310)
 
+
+
+    
 ventana.mainloop()
